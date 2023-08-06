0a61c61610f4a1f55dc069738a6479b62d0f4702
Miranda Eijsackers <mirandaverheij93@gmail.com>
2023-08-06 19:06:40 +0200

    No doubles allowed
    Added method to both simple and hex that finds a new random color that is different from the current number, so that you can never get the same color twice in a row.

--- hex.js
+++ hex.js
@@ -1,4 +1,4 @@
-const hex = [0, 1, 2, 3,4,5,6,7,8,9,"A", "B", "C", "D", "E", "F"];
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 const btn = document.getElementById('btn');
 const color = document.querySelector('.color');
 
@@ -7,15 +7,26 @@ document.body.style.backgroundColor = "#B3ECFF";
 color.textContent = "#B3ECFF";
 
 btn.addEventListener('click', function() {
+    const currentHex = color.textContent;
+    const newHexColor = getDifferentHexColor(currentHex);
+    document.body.style.backgroundColor = newHexColor;
+    color.textContent = newHexColor;
+})
+
+function getDifferentHexColor(currentHex) {
+    const hexColors = new Set();
+    hexColors.add(currentHex);
+    while(hexColors.size !== 2) {
+        hexColors.add(createRandomHexColor());
+    }
+    return [...hexColors][1];
+}
+
+function createRandomHexColor() {
     //Generate a string that consists of a # followed by 6 items of the hex array above
     let hexColor = '#';
     for(let i = 0; i < 6; i++) {
-        hexColor += hex[getRandomNumer()]
-    };
-    document.body.style.backgroundColor = hexColor;
-    color.textContent = hexColor;
-})
-
-function getRandomNumer() {
-    return Math.floor(Math.random() * hex.length);
+        hexColor += hex[Math.floor(Math.random() * hex.length)];
+    }
+    return hexColor;
 }
